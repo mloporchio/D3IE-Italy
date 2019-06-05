e185314a9d3f7740301c78a78f637d70c42d8f18
mloporchio <mloporchio@users.noreply.github.com>
2019-06-05 20:43:17 +0200

    Second beta
    Second beta version.

--- slider.js
+++ slider.js
@@ -5,13 +5,15 @@
 
 var ref = d3.select('#xAxis');
 
+// This function returns the distance between the current position of the xAxis
+// and the margin of the parent container.
 function leftGap() {
     var g1 = d3.select('#graphArea').node().getBoundingClientRect().x;
     var g2 = ref.node().getBoundingClientRect().x;
     return Math.abs(g1 - g2);
 }
 
-var rangeWidth = ref.node().getBoundingClientRect().width - 4;
+var rangeWidth = ref.node().getBoundingClientRect().width - 5;
 var sliderContainer = d3.select('#graph')
     .append('div')
     .attr('id', 'sliderContainer')
@@ -20,7 +22,7 @@ var sliderContainer = d3.select('#graph')
 // Manually set up the resizing behaviour for the slider container.
 d3.select(window)
     .on('resize.sliderContainer', function () {
-        var rangeWidth = ref.node().getBoundingClientRect().width - 4;
+        var rangeWidth = ref.node().getBoundingClientRect().width - 5;
         d3.select('#sliderInput').style('width', (rangeWidth) + 'px');
         d3.select('#sliderContainer').style('padding-left', leftGap() + 'px');
     });
